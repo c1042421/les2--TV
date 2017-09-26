9217af22fe516e07394f8c988ae79bdaa0a021af
c1042421 <c1042421@G-D103-02>
2017-09-26 11:19:41 +0200

    added ui and logic behind the ui

--- oefTV/MainWindow.xaml.cs
+++ oefTV/MainWindow.xaml.cs
@@ -14,15 +14,61 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace oefTV
+
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        TV sony, samsung;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            sony = new TV("Sony", "KDL-40RD450", 50, 40, "sony.png");
+            samsung = new TV("Samsung", "UE55KS7000", 100, 55, "samsung.png");
+        }
+
+        private void RdbSony_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateViewWith(sony);
+        }
+
+        private void RdbSamsung_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateViewWith(samsung);
+        }
+        private void UpdateViewWith(TV tv)
+        {
+            lblInfo.Content = tv.ToString();
+            txtKanaal.Text = tv.Kanaal.ToString();
+            txtVolume.Text = tv.Volume.ToString();
+            imgTV.Source = MakeBitMapImageFor(tv.Afbeelding);
+            cbPower.IsChecked = tv.Power;
+        }
+
+        private BitmapImage MakeBitMapImageFor(string url)
+        {
+            return new BitmapImage(new Uri(@"images\" + url, UriKind.Relative));
+        }
+
+        private void CbPower_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            bool samsungChecked = rdbSamsung.IsChecked ?? false;
+            bool sonyChecked = rdbSony.IsChecked ?? false;
+            bool cbChecked = cb.IsChecked ?? false;
+
+            if (samsungChecked)
+            {
+                samsung.Power = cbChecked;
+            }
+            else if (sonyChecked)
+            {
+                sony.Power = cbChecked;
+            }
         }
     }
 }
---
+++ oefTV/TV.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oefTV
+{ 
+    class TV
+    {
+        private string _afbeelding;
+        private int _beeldgrootte;
+        private int _kanaal;
+        private int _herz;
+        private string _merk;
+        private bool _power;
+        private bool _teletekst;
+        private string _type;
+        private int _volume;
+
+        public TV(string merk, string type, int herz, int beeldgrootte, string afbeelding)
+        {
+            Afbeelding = afbeelding;
+            Beeldgrootte = beeldgrootte;
+            Herz = herz;
+            Merk = merk;
+            Type = type;
+        }
+
+        public TV() : this("", "", 0, 0, "") { }
+
+        public string Afbeelding { get => _afbeelding; set => _afbeelding = value; }
+        public int Beeldgrootte { get => _beeldgrootte; set => _beeldgrootte = value; }
+        public int Herz { get => _herz; set => _herz = value; }
+        public string Merk { get => _merk; set => _merk = value; }
+        public bool Power { get => _power; set => _power = value; }
+        public bool Teletekst { get => _teletekst; set => _teletekst = value; }
+        public string Type { get => _type; set => _type = value; }
+        public int Volume
+        {
+            get => _volume; set
+            {
+                if (Power)
+                {
+                    _volume = value;
+                }
+            }
+        }
+
+        public int Kanaal
+        {
+            get => _kanaal; set
+            {
+                if (Power)
+                {
+                    _kanaal = value;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format("Merk: {0}\nType: {1}\nHerz: {2}\nBeeldgrootte: {3}", Merk, Type, Herz, Beeldgrootte);
+        }
+    }
+}
